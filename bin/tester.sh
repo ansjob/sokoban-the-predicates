2098df5226693ae24edfef3e6d799517b6846533
ansjob <ansjob@eve.(none)>
2012-10-04 16:44:27 +0200

    en fin kommentar

--- bin/tester.sh
+++ bin/tester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {1..100}
 do
-	./timeout3 -t 60 -i 1 java Client dd2380.csc.kth.se 5031 $i
+	./timeout3 -t 60 -i 1 java Client dd2380.csc.kth.se 5032 $i
 	if [[ $? -ne 0 ]] 
 	then
 		echo "TIMEOUT"
--- src/SokobanState.java
+++ src/SokobanState.java
@@ -4,32 +4,45 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
-
 public class SokobanState implements Comparable<SokobanState> {
-	
-	
+
 	public final Set<Coordinate> boxLocations;
-	public final Set<Coordinate> reachableLocations;
+//	public final Set<Coordinate> reachableLocations;
+	public final Set<Coordinate> pushOrPullFromPositions;
 	public final Coordinate currentPosition, pushOrPullFromPosition;
 	public final int priority;
 	public final SokobanState parent;
 	public final boolean isReverse;
 	private final int hashCode;
-	
-	
+
 	public SokobanState(Set<Coordinate> boxLocations,
-			 Coordinate currentPosition, Coordinate pushFromPosition, SokobanState parent, boolean isReverse) {
+			Coordinate currentPosition, Coordinate pushFromPosition,
+			SokobanState parent, boolean isReverse) {
 		this.boxLocations = boxLocations;
 		this.currentPosition = currentPosition;
 		this.pushOrPullFromPosition = pushFromPosition;
 		this.isReverse = isReverse;
+		this.pushOrPullFromPositions = calculatePushOrPullFromPositions();
 		this.parent = parent;
-		this.reachableLocations = getReachablePositions();
+//		this.reachableLocations = getReachablePositions();
 		this.priority = evaluate();
 		this.hashCode = computeHashCode();
 	}
 
-	
+	private Set<Coordinate> calculatePushOrPullFromPositions() {
+		Set<Coordinate> reachablePositions = getReachablePositions();
+		Set<Coordinate> pushOrPullFromPositions = new HashSet<Coordinate>();
+		
+		for (Coordinate box : boxLocations) {
+			for (Coordinate neighbour : box.getNeighbours()) {
+				if (reachablePositions.contains(neighbour)) {
+					pushOrPullFromPositions.add(neighbour);
+				}
+			}
+		}
+		return pushOrPullFromPositions;
+	}
+
 	public int computeHashCode() {
 		final int prime = 31;
 		int result = 1;
@@ -37,11 +50,11 @@ public class SokobanState implements Comparable<SokobanState> {
 				+ ((boxLocations == null) ? 0 : boxLocations.hashCode());
 		result = prime
 				* result
-				+ ((reachableLocations == null) ? 0 : reachableLocations
+				+ ((pushOrPullFromPositions == null) ? 0 : pushOrPullFromPositions
 						.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return hashCode;
@@ -56,15 +69,11 @@ public class SokobanState implements Comparable<SokobanState> {
 		if (getClass() != obj.getClass())
 			return false;
 		SokobanState other = (SokobanState) obj;
-		if (boxLocations == null) {
-			if (other.boxLocations != null)
-				return false;
-		} else if (!boxLocations.equals(other.boxLocations))
+		if (hashCode != other.hashCode)
 			return false;
-		if (reachableLocations == null) {
-			if (other.reachableLocations != null)
-				return false;
-		} else if (!reachableLocations.equals(other.reachableLocations))
+		if (!boxLocations.equals(other.boxLocations))
+			return false;
+		if (!pushOrPullFromPositions.equals(other.pushOrPullFromPositions))
 			return false;
 		return true;
 	}
@@ -73,99 +82,114 @@ public class SokobanState implements Comparable<SokobanState> {
 	public int compareTo(SokobanState o) {
 		return o.priority - priority; // Higher priority first
 	}
-	
+
 	public Set<SokobanState> getChildren() {
 		Set<SokobanState> children = new HashSet<SokobanState>();
 		for (Coordinate box : boxLocations) {
 			for (Coordinate from : box.getNeighbours()) {
-				if ((!isReverse && reachableLocations.contains(from) && box.isPushableFrom(from, boxLocations)) || (isReverse && reachableLocations.contains(from) && box.canBePulledFrom(from, boxLocations))) {
-					Coordinate playerPosition = (isReverse ? from.push(box) : box), newBoxPosition = (isReverse ? from : box.push(from));
+				if ((!isReverse && pushOrPullFromPositions.contains(from) && box
+						.isPushableFrom(from, boxLocations))
+						|| (isReverse && pushOrPullFromPositions.contains(from) && box
+								.canBePulledFrom(from, boxLocations))) {
+					Coordinate playerPosition = (isReverse ? from.push(box)
+							: box), newBoxPosition = (isReverse ? from : box
+							.push(from));
 					Set<Coordinate> newBoxPositions = new HashSet<Coordinate>();
 					newBoxPositions.addAll(boxLocations);
 					newBoxPositions.remove(box);
 					newBoxPositions.add(newBoxPosition);
-					SokobanState child = new SokobanState(newBoxPositions, playerPosition, from, this, isReverse);
+					SokobanState child = new SokobanState(newBoxPositions,
+							playerPosition, from, this, isReverse);
 					if (!child.isDead())
 						children.add(child);
 				}
 			}
 		}
 		return children;
-	}	
+	}
 
 	private int evaluate() {
 		int val = 0;
-		for (Coordinate goal : (isReverse ? SokobanBoard.startBoxPositions : SokobanBoard.goalPositions)) {
+		for (Coordinate goal : (isReverse ? SokobanBoard.startBoxPositions
+				: SokobanBoard.goalPositions)) {
 			if (boxLocations.contains(goal))
 				val += 5;
 		}
 
-		val += 1*distanceValue();
+		val += distanceValue();
 
-		if(isReverse){
+		if (isReverse) {
 			return val;
 		}
-		
+
 		for (Coordinate box : boxLocations) {
 			for (Coordinate from : box.getNeighbours()) {
-				if (reachableLocations.contains(from) && box.isPushableFrom(from, boxLocations)) {
+				if (pushOrPullFromPositions.contains(from)
+						&& box.isPushableFrom(from, boxLocations)) {
 					val++;
 				}
 			}
 		}
-		
+
 		return val;
 	}
-	
-	private int distanceValue(){
+
+	private int distanceValue() {
 		int val = 0;
-		
-		for (Coordinate box : boxLocations){
+
+		// Max is the diagonal on the board
+		int maxDistance = boxLocations.size() * SokobanBoard.cells.length
+				+ SokobanBoard.cells[0].length;
+
+		for (Coordinate box : boxLocations) {
 			int dist = Integer.MAX_VALUE;
-			for (Coordinate goal : (isReverse ? SokobanBoard.startBoxPositions : SokobanBoard.goalPositions)){
-				dist = Math.min(dist, (int)Math.sqrt((box.col + goal.col)*(box.col + goal.col) + (box.row + goal.row)*(box.row + goal.row)));
+			for (Coordinate goal : (isReverse ? SokobanBoard.startBoxPositions
+					: SokobanBoard.goalPositions)) {
+				dist = Math.min(
+						dist, Math.abs(box.col - goal.col)
+						+ Math.abs(box.row - box.col));
 			}
 			val += dist;
 		}
-		
-		return val;
+
+		return 100 * (int) ((float) maxDistance / (float) val);
 	}
 
 	private boolean isDead() {
 		return false;
-//		for (Coordinate box : boxLocations){
-//			boolean up = boxLocations.contains(box.up());
-//			boolean down = boxLocations.contains(box.down());
-//			boolean left;
-//			boolean right;
-//			if (!up && ! down){
-//				continue;
-//			}
-//				
-//			
-//			left = boxLocations.contains(box.left());
-//			right = boxLocations.contains(box.right());
-//			if(!left && !right){
-//				continue;
-//			}
-//			
-//			if(left){
-//				if(up && boxLocations.contains(box.up().left())){
-//					return true;
-//				} 
-//				if (down && boxLocations.contains(box.down().left())){
-//					return true;
-//				}
-//					
-//			}
-//			if(right){
-//				if (up && boxLocations.contains(box.up().right())){
-//					return true;
-//				}
-//				if (down && boxLocations.contains(box.down().right()));
-//			}
-//		}
-//		return false;
+		// for (Coordinate box : boxLocations){
+		// boolean up = boxLocations.contains(box.up());
+		// boolean down = boxLocations.contains(box.down());
+		// boolean left;
+		// boolean right;
+		// if (!up && ! down){
+		// continue;
+		// }
+		//
+		//
+		// left = boxLocations.contains(box.left());
+		// right = boxLocations.contains(box.right());
+		// if(!left && !right){
+		// continue;
+		// }
+		//
+		// if(left){
+		// if(up && boxLocations.contains(box.up().left())){
+		// return true;
+		// }
+		// if (down && boxLocations.contains(box.down().left())){
+		// return true;
+		// }
+		//
+		// }
+		// if(right){
+		// if (up && boxLocations.contains(box.up().right())){
+		// return true;
+		// }
+		// if (down && boxLocations.contains(box.down().right()));
+		// }
+		// }
+		// return false;
 	}
 
 	private Set<Coordinate> getReachablePositions() {
@@ -173,13 +197,12 @@ public class SokobanState implements Comparable<SokobanState> {
 		Queue<Coordinate> q = new LinkedList<Coordinate>();
 		q.add(currentPosition);
 		result.add(currentPosition);
-		while (!q.isEmpty()){
+		while (!q.isEmpty()) {
 			Coordinate cur = q.remove();
 			for (Coordinate neighbour : cur.getNeighbours()) {
-				if (SokobanBoard.cells[neighbour.row][neighbour.col] != '#' 
+				if (SokobanBoard.cells[neighbour.row][neighbour.col] != '#'
 						&& !boxLocations.contains(neighbour)
-						&& !result.contains(neighbour))
-				{
+						&& !result.contains(neighbour)) {
 					result.add(neighbour);
 					q.add(neighbour);
 				}
@@ -187,7 +210,7 @@ public class SokobanState implements Comparable<SokobanState> {
 		}
 		return result;
 	}
-	
+
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		for (int row = 0; row < SokobanBoard.cells.length; row++) {
@@ -197,29 +220,26 @@ public class SokobanState implements Comparable<SokobanState> {
 				if (boxLocations.contains(currentPos)) {
 					if (c == '.') {
 						c = '*';
-					}
-					else {
+					} else {
 						c = '$';
 					}
 				}
 				if (currentPos.equals(this.currentPosition)) {
 					if (c == '.') {
 						c = '+';
-					}
-					else {
+					} else {
 						c = '@';
 					}
 				}
-//				if (c != '#' && SokobanBoard.staticDead[row][col]){
-//					c = ',';
-//				}
+				// if (c != '#' && SokobanBoard.staticDead[row][col]){
+				// c = ',';
+				// }
 				sb.append(c);
 			}
 			sb.append('\n');
 		}
-		
+
 		return sb.toString();
 	}
-	
-	
+
 }
